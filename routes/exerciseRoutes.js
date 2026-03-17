1240f93392cc14a2c3daa42e6d745f137b8b3503
kunafina-ruslana <kunafina.rr@gmail.com>
2026-03-17 10:08:16 +0300

    Update exerciseRoutes.js

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -14,33 +14,14 @@ const router = express.Router();
 
 const exerciseValidation = [
   body('name').notEmpty().withMessage('Название обязательно'),
-  body('description').notEmpty().withMessage('Описание обязательно'),
-  body('rutubeFullUrl')
-    .optional()
-    .custom((value) => {
-      if (!value) return true;
-      const patterns = [
-        /rutube\.ru\/video\/([a-f0-9]+)/i,
-        /rutube\.ru\/play\/embed\/([a-f0-9]+)/i
-      ];
-      const isValid = patterns.some(pattern => pattern.test(value));
-      if (!isValid) throw new Error('Неверный формат ссылки Rutube');
-      return true;
-    }),
-  body('rutubeStartTime').optional().isInt({ min: 0 }),
-  body('rutubeEndTime')
-    .optional()
-    .custom((value, { req }) => {
-      if (value && req.body.rutubeStartTime && value <= req.body.rutubeStartTime) {
-        throw new Error('Время окончания должно быть больше времени начала');
-      }
-      return true;
-    })
+  body('description').notEmpty().withMessage('Описание обязательно')
 ];
 
+// Публичные маршруты
 router.get('/', getAllExercises);
 router.get('/:id', getExerciseById);
 
+// Защищенные маршруты для тренера и админа
 router.get('/my', authenticate, authorize('trainer', 'admin'), getMyExercises);
 router.post('/', authenticate, authorize('trainer', 'admin'), exerciseValidation, createExercise);
 router.put('/:id', authenticate, authorize('trainer', 'admin'), exerciseValidation, updateExercise);
