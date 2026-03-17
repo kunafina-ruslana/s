4d4ae73a157bd2cc4e38da20c57715425199ec6c
kunafina-ruslana <kunafina.rr@gmail.com>
2026-03-17 09:26:51 +0300

    Update exerciseRoutes.js

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -4,6 +4,7 @@ import { authenticate, authorize } from '../middleware/authMiddleware.js';
 import {
   getAllExercises,
   getExerciseById,
+  getMyExercises,
   createExercise,
   updateExercise,
   deleteExercise
@@ -20,7 +21,7 @@ const exerciseValidation = [
       if (!value) return true;
       const patterns = [
         /rutube\.ru\/video\/([a-f0-9]+)/i,
-        /rutube\.ru\/play\/([a-f0-9]+)/i
+        /rutube\.ru\/play\/embed\/([a-f0-9]+)/i
       ];
       const isValid = patterns.some(pattern => pattern.test(value));
       if (!isValid) throw new Error('Неверный формат ссылки Rutube');
@@ -39,9 +40,9 @@ const exerciseValidation = [
 
 router.get('/', getAllExercises);
 router.get('/:id', getExerciseById);
+router.get('/my', authenticate, authorize('trainer', 'admin'), getMyExercises);
 router.post('/', authenticate, authorize('trainer', 'admin'), exerciseValidation, createExercise);
 router.put('/:id', authenticate, authorize('trainer', 'admin'), exerciseValidation, updateExercise);
 router.delete('/:id', authenticate, authorize('trainer', 'admin'), deleteExercise);
-router.get('/my', authenticate, authorize('trainer', 'admin'), getMyExercises);
 
 export default router;
